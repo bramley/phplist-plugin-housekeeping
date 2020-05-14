6bc8e08c7f6cfed269e378a50f6c47495f7b3de3
Duncan Cameron <phplist@dcameron.me.uk>
2020-05-14 13:38:53 +0100

    Delete duplicate bounces from user_message_bounce

--- plugins/HousekeepingPlugin/deletebounces.php
+++ plugins/HousekeepingPlugin/deletebounces.php
@@ -11,9 +11,23 @@
  * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License, Version 3
  */
 
-/*
- * Delete all bounces by truncating the bounce table
- */
+// delete duplicate bounces from user_message_bounce
+$query = <<<END
+DELETE FROM {$tables['user_message_bounce']}
+WHERE id NOT IN (
+     SELECT * FROM (
+        SELECT MIN(id)
+        FROM {$tables['user_message_bounce']}
+        GROUP BY user, message
+    ) t
+)
+END;
 
-Sql_Query(sprintf('delete from %s', $tables['bounce']));
-cl_output('All bounces have been deleted');
+Sql_Query($query);
+$rows = Sql_Affected_Rows();
+cl_output(sprintf('%d rows from the user_message_bounce table have been deleted', $rows));
+
+// delete all bounces
+Sql_Query("delete from {$tables['bounce']}");
+$rows = Sql_Affected_Rows();
+cl_output(sprintf('%d rows from the bounce table have been deleted', $rows));
