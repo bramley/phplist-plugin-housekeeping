b0a58b295b14ca1f113f12dd5992719f03fa35db
Duncan Cameron <phplist@dcameron.me.uk>
2020-10-17 17:42:04 +0100

    Remove the query to delete duplicate bounces because it was too intensive.

--- plugins/HousekeepingPlugin/deletebounces.php
+++ plugins/HousekeepingPlugin/deletebounces.php
@@ -11,22 +11,6 @@
  * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License, Version 3
  */
 
-// delete duplicate bounces from user_message_bounce
-$query = <<<END
-DELETE FROM {$tables['user_message_bounce']}
-WHERE id NOT IN (
-     SELECT * FROM (
-        SELECT MIN(id)
-        FROM {$tables['user_message_bounce']}
-        GROUP BY user, message
-    ) t
-)
-END;
-
-Sql_Query($query);
-$rows = Sql_Affected_Rows();
-cl_output(sprintf('%d rows from the user_message_bounce table have been deleted', $rows));
-
 // delete all bounces
 Sql_Query("delete from {$tables['bounce']}");
 $rows = Sql_Affected_Rows();
